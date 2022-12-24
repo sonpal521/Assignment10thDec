1a8f6c9df328200e8b8822adc7a768d83ea92576
sonpal521 <119480099+sonpal521@users.noreply.github.com>
2022-12-24 08:24:24 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,31 @@
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+// Given an array having 0,1 as input. We need to write a function that sorts the array in 
+//ascending order without using inbuilt sort function
+// Input Array -> [0,1,1,0,1,0,0,1]
+// Output -> [0,0,0,0,1,1,1,1]
+
+function separate(arr) {
+    let i = 0;
+    let j = arr.length - 1;
+    while(i <= j) {
+        // till the time i and j have not passed each other
+        if(arr[i] == 1) { // we just check if we have 1 at index i, doesnt matter what we have at j
+            swap(arr, i, j);
+            j--; // include the new 1 we threw in the right window
+        } else {
+            i++; // expand the left window directly
+        }
+    }
+}
+
+let arr = [1,1,1,0,1,0,0,0,1,0,1];
+separate(arr); // transform our original array
+console.log(arr);
+//output->[0, 0, 0, 0, 0,1, 1, 1, 1, 1,1]
+
+
